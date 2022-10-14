384b3469a01f8dee1c32677b0d00fa476cca0007
Iagosilva019 <iagosilvasantana21@gmail.com>
2022-10-14 11:25:38 -0300

    Estrutura do projeto

---
+++ pessoa/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import ListaPessoaViews
+
+urlpatterns = [
+    path('',ListaPessoaViews.as_view(), name='pessoa.index'),
+]
---
+++ pessoa/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Pessoa
+
+
+# Create your views here.
+class ListaPessoaViews(ListView):
+    model = Pessoa
+    queryset = Pessoa.objects.all().order_by('nome_completo')
